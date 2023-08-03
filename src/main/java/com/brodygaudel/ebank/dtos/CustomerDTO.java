a0ecedbca71cb9ba482cff989195e442c166c27f
Brody Gaudel <57298219+BrodyGaudel@users.noreply.github.com>
2023-08-04 00:51:55 +0100

    final

---
+++ src/main/java/com/brodygaudel/ebank/dtos/CustomerDTO.java
@@ -0,0 +1,17 @@
+
+
+package com.brodygaudel.ebank.dtos;
+
+import com.brodygaudel.ebank.enums.Sex;
+
+import java.util.Date;
+
+public record CustomerDTO(
+        Long id,
+        String firstname,
+        String name,
+        Date dateOfBirth,
+        String placeOfBirth,
+        String nationality,
+        String cin,
+        Sex sex) { }
